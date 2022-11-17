edd9383df85cbeaaf2da5b9c448b56caffcfbe84
AhmedMousa-ag <ahmedkaremag@gmail.com>
2022-11-17 14:22:00 +0200

    updated config

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 import os
 
 PREPROCESS_ARTIFACT_PATH = os.path.join("Utils","preprocess","artifacts")
-if not os.path.exists(preprocess_artifact_path):
-    os.makedirs(preprocess_artifact_path)
+if not os.path.exists(PREPROCESS_ARTIFACT_PATH):
+    os.makedirs(PREPROCESS_ARTIFACT_PATH)
 
 
 HYPER_PARAM_PATH = os.path.join() #TODO
@@ -10,3 +10,7 @@ HYPER_PARAM_PATH = os.path.join() #TODO
 DATA_SCHEMA_PATH = os.path.join() #TODO
 
 DATA_PATH = os.path.join() #TODO
+
+MODEL_NAME= "RNN_pretrained_embed.h5"
+
+MODEL_SAVE_PATH = os.path.join() #TODO
